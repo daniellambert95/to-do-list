ee3f4bd3e0d69c46b2609d6f98bdb50b608a9e11
temporaryadmin <danjlambert95@gmail.com>
2022-02-22 11:32:29 +0100

    initial commit

---
+++ todo_list/base/views.py
@@ -0,0 +1,91 @@
+from asyncio import Task
+from encodings import search_function
+from django.shortcuts import redirect, render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from .models import Task
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
+
+class UserLogin(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    # common to be False, we could have the login page with a link to a sing up and login
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    # redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+
+# this function is logging in the newly registered user if the form is valid
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+# will redirect the user if they are logged in / registered
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+# the login required mixin will make sure the user needs to be logged in to view his task list
+# we have set the LOGIN_URL in the settings so that ir will redirect to the login if someone tries to view this page
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+#  This function below is an override that is returning filtered data to a specific user due to the self.request.user filter
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+# Backend for the search form
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains=search_input)
+            
+        context['search_input'] = search_input
+
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
